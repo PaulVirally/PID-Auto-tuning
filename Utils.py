eaa303ce0246ed23a574b2efd1b8886df1d82e7c
PaulVirally <pvirally@gmail.com>
2019-08-04 17:09:01 +0200

    Added clamp and signum

--- Utils.py
+++ Utils.py
@@ -12,3 +12,35 @@ def remap(x: float, min_x: float, max_x: float, min_y: float, max_y: float) -> f
         float: The remapped value ranging from `min_y` to `max_y`.
     """
     return min_y + (max_y - min_y) * ((x - min_x) / (max_x - min_x))
+
+def clamp(x: float, min_x: float, max_x: float) -> float:
+    """Clamps `x` to `min_x` on the bottom and `max_x` on the top.
+    
+    Arguments:
+        x {float} -- The value to clamp.
+        min_x {float} -- The floor of the clamp.
+        max_x {float} -- The ceiling of the clamp.
+    
+    Returns:
+        float -- The new clamped value.
+    """
+    if x < min_x:
+        return min_x
+    elif x > max_x:
+        return max_x
+    return x
+
+def signum(x: float) -> float:
+    """Returns the sign of `x`.
+    
+    Arguments:
+        x {float} -- The value to be tested.
+    
+    Returns:
+        float -- Returns +1.0 if `x` > 0, -1.0 if `x` < 0, and 0.0 if `x` == 0.
+    """
+    if x < 0:
+        return -1.0
+    elif x > 0:
+        return 1.0
+    return 0.0
