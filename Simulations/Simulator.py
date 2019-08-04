1c5731ac3d649aac632cffd3b9d20c66df9f2f05
PaulVirally <pvirally@gmail.com>
2019-08-04 16:49:35 +0200

    Added a max and min to the input of the system

--- Simulations/Simulator.py
+++ Simulations/Simulator.py
@@ -1,11 +1,21 @@
 class Simulator:
     """Abstract class for all simulations to provide a consistent API."""
 
-    def set_input(self, val):
+    def __init__(self, min_input: float, max_input: float):
+        """Constructs a Simulator with inputs varyinng between `min_input` and `max_input`.
+        
+        Arguments:
+            min_input {float} -- The minimum value the Simulator can take in as an input.
+            max_input {float} -- The maximum value the Simulator can take in as an input.
+        """
+        self.min_input = min_input
+        self.max_input = max_input
+
+    def set_input(self, val: float):
         """Set the input to the Simulator.
 
         Arguments:
-            val -- The value the Simulator takes in as its input.
+            val {float} -- The value the Simulator takes in as its input.
         """
         raise NotImplementedError
 
