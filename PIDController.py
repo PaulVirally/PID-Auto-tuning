7e477cc0062ce103292d94fe4f67fcad839fae87
PaulVirally <pvirally@gmail.com>
2019-08-05 12:06:10 +0200

    Added a PIDController.

--- PIDController.py
+++ PIDController.py
@@ -1,26 +1,60 @@
+from Simulations import Simulator
+
 class PIDController:
     """Controller for a system using a PID loop."""
 
-    def __init__(self, kp: float, ki: float, kd: float):
-        """Constructs the PIDController
+    def __init__(self, sim: Simulator, setpoint: float, kp: float, ki: float, kd: float):
+        """Constructs a PIDController.
 
         Arguments:
-            kp {float} -- Proportional constant in PID
-            ki {float} -- Integral constant in PID
-            kd {float} -- Derivative connstant in PID
+            sim {Simulator} -- The Simulator the controller will use.
+            setpoint {float} -- The target value the Simulator must reach.
+            kp {float} -- Proportional constant in PID.
+            ki {float} -- Integral constant in PID.
+            kd {float} -- Derivative connstant in PID.
         """
+        self.sim = sim
+        self.setpoint = setpoint
         self.kp = kp
         self.ki = ki
         self.kd = kd
 
-    def my_func(self, a: float, b: float) -> float:
-        """[summary]
+        self._previous_value = 0 # Used for the derivative term
+        self._integral = 0 # Used for the integral term
+
+    def get_error(self) -> float:
+        return self.setpoint - self.sim.get_output()
+
+    def _get_input(self, dt: float) -> float:
+        """Computes the P, I, and D terms to get the input that should be given to the simulated system.
         
         Arguments:
-            a {float} -- [description]
-            b {float} -- [description]
-        
+            dt {float} -- The time interval between time steps.
+
         Returns:
-            float -- [description]
+            float -- The input that should be given to the system.
         """
-        return a * self.kp + b
+        err = self.get_error()
+
+        p = self.kp * err
+
+        self._integral += err * dt
+        i = self.ki * self._integral
+
+        d = self.kd * (self.sim.get_output() - self._previous_value)/dt
+
+        return p + i + d
+
+    def step(self, dt: float):
+        """Takes a step in time for the PIDController.
+
+        The P, I, and D terms are calculated to find the new input to give to the system.
+        The simulated system is then updated according to the computed input.
+        
+        Arguments:
+            dt {float} -- [description]
+        """
+        inpt = self._get_input(dt)
+        self.sim.set_input(inpt)
+        self._previous_value = self.sim.get_output()
+        self.sim.step(dt)
